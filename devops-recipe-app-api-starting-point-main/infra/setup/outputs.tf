9b2ef3606c62fcbc9c9846f975b7c479a60ea8d4
Happy B <bricehappy@gmail.com>
2025-02-28 15:29:53 +0100

    Addes iam user for backend ci cd

---
+++ devops-recipe-app-api-starting-point-main/infra/setup/outputs.tf
@@ -0,0 +1,14 @@
+output "cd_user_access_key_id" {
+
+  description = "AWS key ID for CD User"
+  value       = aws_iam_access_key.cd.id
+
+}
+
+output "cd_user_access_key_secret" {
+
+  description = "AWS key secret for CD User"
+  value       = aws_iam_access_key.cd.secret
+  sensitive   = true
+
+}
